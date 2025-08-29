1493a5f8323240db0681d22c0017419fb8d12877
Catriel Cabrera <90102633+catriel458@users.noreply.github.com>
2025-08-29 15:01:38 -0300

    Update Contact.tsx

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -86,7 +86,7 @@ const Contact = () => {
   };
 
   return (
-    <div className="container py-20">
+    <div className="container mx-auto px-4 w-full max-w-7xl py-20">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
