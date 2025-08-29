23cdb7033b8e07e7f9d4e846da9aabb360405aab
Catriel Cabrera <90102633+catriel458@users.noreply.github.com>
2025-08-29 14:55:21 -0300

    Update About.tsx

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -20,7 +20,7 @@ const About = () => {
   ];
 
   return (
-    <div className="container py-20">
+    <div className="container mx-auto px-4 w-full max-w-7xl py-20">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
